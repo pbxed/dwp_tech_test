0d951d918349cbd58d2c542617739c93e6b7cd96
pbxed <jshenolikar@gmail.com>
2019-11-28 13:09:11 +0000

    implemented custom exception handling as standardised json response.

---
+++ src/main/java/com/dwp/error/CustomGlobalExceptionHandler.java
@@ -0,0 +1,55 @@
+package com.dwp.error;
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({ConstraintViolationException.class})
+    public ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException ex, WebRequest request) {
+        List<String> errors = new ArrayList<String>();
+
+        for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+            errors.add(violation.getMessage());
+        }
+
+        ApiError apiError =
+                new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errors);
+        return new ResponseEntity<Object>(
+
+                apiError, new HttpHeaders(), apiError.getStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(ex.getMethod());
+        builder.append(" method is not supported for this request. Supported methods are ");
+        ex.getSupportedHttpMethods().forEach(t -> builder.append(t + " "));
+
+        ApiError apiError = new ApiError(HttpStatus.METHOD_NOT_ALLOWED, ex.getLocalizedMessage(), builder.toString());
+
+        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
+    }
+
+    @ExceptionHandler({ Exception.class })
+    public ResponseEntity<Object> handleAll(Exception ex, WebRequest request) {
+        ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex.getLocalizedMessage(), "error occurred");
+
+        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
+    }
+}
+
--- src/main/java/com/dwp/presentation/UserController.java
+++ src/main/java/com/dwp/presentation/UserController.java
@@ -3,14 +3,24 @@ package com.dwp.presentation;
 import com.dwp.data.model.User;
 import com.dwp.service.UserService;
 import io.swagger.annotations.Api;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Api
 @RestController
 @RequestMapping("/api")
+@Validated
 public class UserController {
 
     private final UserService userService;
@@ -22,10 +32,10 @@ public class UserController {
 
     @GetMapping(path = "/users")
     public List<User> getUsersByCityAndDistanceFromCity(
-            @RequestParam(value = "city") String city,
-            @RequestParam(value = "lat") double latitude,
-            @RequestParam(value = "long") double longitude,
-            @RequestParam(value = "dist") double distanceInMiles) {
+            @RequestParam(value = "city") @NotNull @Size(min = 2) String city,
+            @RequestParam(value = "lat") @NotNull @Max(90) @Min(-90) double latitude,
+            @RequestParam(value = "long") @NotNull @Max(180) @Min(-180) double longitude,
+            @RequestParam(value = "dist") @NotNull @Max(7926) @Min(0) double distanceInMiles) {
         return userService.getUsersByDistanceFromCoordinate(city, latitude, longitude, distanceInMiles);
     }
 }
